948410af4abbd89d254bd07e07230a14a44b8c1d
Mark <blackbirdstoragellc@gmail.com>
2019-12-09 21:47:59 -0500

    Updated themes and urls in all the links

--- styleCreator.js
+++ styleCreator.js
@@ -28,8 +28,31 @@ switch(['gmu', 'uva', 'jmu', 'umw', 'vt', 'ru'].indexOf(school)) {
 
 let els = document.querySelectorAll('.themedElement');
 
-console.log(els);
-
 els.forEach(function(el) {
     el.classList.add(theme);
+});
+
+select_element = document.getElementById('dynamic_select');
+if (select_element != null) {
+    select_element.value = theme;
+}
+
+home_btn = document.getElementById('home_btn');
+if (home_btn != null) {
+    home_btn.href="index.html?school=" + theme;
+}
+
+pricing_btn = document.getElementById('pricing_btn');
+if (pricing_btn != null) {
+    pricing_btn.href="index.html?school=" + theme + "#pricing";
+}
+
+blog_btn = document.getElementById('blog_btn');
+if (blog_btn != null) {
+    blog_btn.href="blog.html?school=" + theme;
+}
+
+let btns = document.querySelectorAll('.reserve_btn');
+btns.forEach(function(el) {
+    el.href="signup.html?school=" + theme;
 });
